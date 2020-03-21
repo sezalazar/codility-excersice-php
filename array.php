98d295f8163fe6c585cfc7beac20bd9a7bf37bdd
sezalazar <53580787+sezalazar@users.noreply.github.com>
2020-03-21 16:50:22 -0300

    Add files via upload

--- array.php
+++ array.php
@@ -1,13 +1,43 @@
 <?php
 function solution($A) 
 {
-    // write your code in PHP7.0
 
+
+    // write your code in PHP7.0
+   /*
+    Write a function:
+
+    function solution(A);
+
+    that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+    
+    For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+    
+    Given A = [1, 2, 3], the function should return 4.
+    
+    Given A = [−1, −3], the function should return 1.
+    
+    Assume that:
+    
+        N is an integer within the range [1..100,000]
+        Each element of array A is an integer within the range [−1,000,000..1,000,000]
+    
+    Complexity:
+    
+        Expected worst-case time complexity is O(N)
+    
+    Expected worst-case space complexity is O(N)
+    , beyond input storage (not counting the storage required for input arguments)
+
+    */
+    
     $min = min($A);
     $minPosition = array_search($min, $A);
 
     unset($A[$minPosition]);
     $count = count($A);
+
+
     
     for ($i = 0; $i < $count; $i++) 
     {
